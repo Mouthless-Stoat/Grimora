9bf0ec722cb7898938b767b5b5d85815d2e2c4d3
Mouthless Stoat <nphuyanh@outlook.com>
2024-06-03 22:37:12 -0300

    Missing )

--- src/trans.rs
+++ src/trans.rs
@@ -1,7 +1,7 @@
 use crate::parser::Node;
 
 pub fn trans(program: Vec<Node>) -> String {
-    let mut out = String::from("extends SigilEffect\nfunc handle_event(event, params:\n");
+    let mut out = String::from("extends SigilEffect\nfunc handle_event(event, params):\n");
     if program.len() <= 0 {
         return out + "\tpass";
     }
